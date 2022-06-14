ebd75014dc7c61e70ee3aa8f70a94e7bd7849173
alx2009 <100997527+alx2009@users.noreply.github.com>
2022-06-14 23:47:48 +0200

    Added note clarifying the SS statement for AVR Dx

---
+++ SPIport.h
@@ -0,0 +1,10 @@
+// Include definitions to identify WHAT SPI port is used
+// the idea is to be able to support even the SPI MAster included in the UART
+// in order to have a dedicated SPI interface to the SD card 
+
+#ifndef SPI_PORT_H
+#define SPI_PORT_H
+
+#define SPI_PORT SPI0
+
+#endif //SPI_PORT_H
