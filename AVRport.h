6bc7ede87ef8abc17e0312a76e969d640fdf11b5
alx2009 <100997527+alx2009@users.noreply.github.com>
2022-06-18 09:25:16 +0200

    Added AVR_TCB_PORT definition

--- AVRport.h
+++ AVRport.h
@@ -2,9 +2,29 @@
 // the idea is to be able to support even the SPI MAster included in the UART
 // in order to have a dedicated SPI interface to the SD card 
 
+//TIMERS AVR128DBxx and dxCore usage:
+// TCA0  ==> PWM 
+// TCA1* ==> PWM 
+// TCB0  ==> Tone
+// TCB1  ==> Servo library
+// TCB2  ==> millis
+// TCB3* 
+// TCD0 ==> PWM
+// RTC
+
+* not availble for 28 and 32 pin AVRs
+
+
 #ifndef AVR_PORT_H
 #define AVR_PORT_H
 
-#define AVR_PORT SPI0
+#define AVR_SPI_PORT SPI0
+
+#ifdef DB_28_PINS
+#   define AVR_TCB_PORT   TCB1
+#else
+#   define AVR_TCB_PORT   TCB3
+#endif
+
 
 #endif //AVR_PORT_H
